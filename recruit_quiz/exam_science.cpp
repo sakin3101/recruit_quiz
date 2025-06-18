25844c47cf6907a2259a8c55a93fade3332ff24c
sakin3101 <atsuki.k.171013@gmail.com>
2025-06-18 14:52:13 +0900

    等速直線運動の問題が2つ出題されるように変更
    ・問題文が正しくない問題を修正
    ・平均時速を求める問題を追加

--- recruit_quiz/exam_science.cpp
+++ recruit_quiz/exam_science.cpp
@@ -19,10 +19,17 @@ QuestionList CreatePhysicsExam()
 		int t = uniform_int_distribution<>(1, 10)(rd) * divisors[i];	// 時間
 		questions.push_back({
 			"時速" + to_string(v) + "kmで移動する車がある。\nこの車が" + to_string(v * t / 60) +
-			to_string(t) + "km移動するのにかかる時間を分単位で求めよ"});
+			+ "km移動するのにかかる時間を分単位で求めよ",
+			to_string(t)});
+
+		i = uniform_int_distribution<>(0, size(divisors) - 1)(rd);
+		v = uniform_int_distribution<>(1, 10)(rd) * 60 / divisors[i];	// 時速
+		t = uniform_int_distribution<>(1, 10)(rd) * divisors[i];	// 時間
+		questions.push_back({
+			"ある車が" + to_string(v * t / 60) + "km離れた地点まで移動するのに" + to_string(t) +
+			"分かかった。\nこの車の平均時速を求めよ",
+			to_string(v) });
 	}	// 等速直線運動
-
-
 
 	return questions;
 }
