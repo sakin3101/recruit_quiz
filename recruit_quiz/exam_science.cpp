5ad33a7365417fe30c050a27951d405d7b3005d7
sakin3101 <atsuki.k.171013@gmail.com>
2025-07-02 15:41:40 +0900

    ばねの問題にばねの変化量の合計を求める問題を追加。

--- recruit_quiz/exam_science.cpp
+++ recruit_quiz/exam_science.cpp
@@ -165,6 +165,25 @@ QuestionList CreatePhysicsExam()
 			to_string(a + x) + "cmになった。\n"
 			"このばねの「ばね定数」を小数点以下第2位を四捨五入して求めよ。",
 			answer });
+
+		int k1 = uniform_int_distribution<>(1, 10)(rd);	// ばね定数1(N/m)
+		int k2 = uniform_int_distribution<>(1, 9)(rd);	// ばね定数2(N/m)
+		if(k2 >= k1) {
+			k2++;
+		}
+		m = uniform_int_distribution<>(1, 10)(rd) * 10;	// 質量(g)
+		x = 100 * m * (k1 + k2) / (k1 * k2) + 5;
+		answer = to_string(x / 100);
+		x /= 10;
+		if (x % 10) {
+			answer += '.';
+			answer += '0' + x % 10;
+		}
+		questions.push_back({
+			"重力加速度を10m/s^2とする。\nばね定数が" + to_string(k1) + "と" + to_string(k2) +
+			"の2つのばねを直列につなぎ、" + to_string(m) + "gの重りをつけて天井から吊り下げた。\n" +
+			"Xの値を小数点以下第2位を四捨五入して求めよ。",
+			answer });
 	}	// ばね
 
 	return questions;
